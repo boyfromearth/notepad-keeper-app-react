eebd6892769ccab5326fdfc2484d8d6554d12324
hanushs <hanushs@gmail.com>
2025-05-02 22:02:33 -0400

    adding source code

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch locally project in your browser
